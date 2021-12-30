f9387dc2fb3983a0dffac6084b9bef0eb5a40411
Robin Malhotra <me@rmalhotra.com>
2021-12-30 00:03:07 +0000

    Add palindrome number

---
+++ MSFTInterviewQuestions/PalindromeNumber.swift
@@ -0,0 +1,41 @@
+//
+//  PalindromeNumber.swift
+//  MSFTInterviewQuestions_macOS
+//
+//  Created by Robin Malhotra on 29/12/2021.
+//
+
+import Foundation
+
+import Foundation
+
+//TODO: Revise this
+
+class PalindromeNumberSolution {
+    func isPalindrome(_ x: Int) -> Bool {
+        var l = x
+        var r = 0
+        
+        if x < 0 {
+            return false
+        }
+        
+        if (x % 10 == 0 && x != 0) {
+            return false
+        }
+        
+        while l > r {
+            r = r * 10 + l % 10
+            l = l / 10
+        }
+        
+        return (l == r) || (l == r / 10)
+    }
+    
+    /*
+     1221, 0, 1221
+     122 , 1, 1221
+     12 , 12, 122
+     */
+    
+}
---
+++ MSFTInterviewQuestionsTests/PalindromeNumberTest.swift
@@ -0,0 +1,34 @@
+//
+//  PalindromeNumberTest.swift
+//  MSFTInterviewQuestions_macOS
+//
+//  Created by Robin Malhotra on 29/12/2021.
+//
+
+import Foundation
+
+import XCTest
+@testable import MSFTInterviewQuestions
+
+
+class PalindromeNumberTest: XCTestCase {
+    func testSolver1() {
+        let solver = PalindromeNumberSolution()
+        XCTAssertTrue(solver.isPalindrome(121))
+    }
+    
+    func testSolver2() {
+        let solver = PalindromeNumberSolution()
+        XCTAssertFalse(solver.isPalindrome(10))
+    }
+    
+    func testSolver3() {
+        let solver = PalindromeNumberSolution()
+        XCTAssertTrue(solver.isPalindrome(1021111201))
+    }
+    
+    func testSolver4() {
+        let solver = PalindromeNumberSolution()
+        XCTAssertFalse(solver.isPalindrome(12345))
+    }
+}
