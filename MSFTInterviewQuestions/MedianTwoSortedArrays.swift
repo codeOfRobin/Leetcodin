d445222415127d1fc8a7ed3b858fc8ba8c3632a6
Robin Malhotra <me@rmalhotra.com>
2022-01-01 00:25:48 +0000

    Add median array

--- MSFTInterviewQuestions/MedianTwoSortedArrays.swift
+++ MSFTInterviewQuestions/MedianTwoSortedArrays.swift
@@ -12,30 +12,39 @@ import Foundation
 class MedianTwoSortedArrays {
     
     func findMedianSortedArrays(_ nums1: [Int], _ nums2: [Int]) -> Double {
-        let (a, b) = nums1.count < nums2.count ? (nums1, nums2) : (nums2, nums1)
-        let half = (nums1.count + nums2.count) / 2
+        // as an example, [1,3,5,7,9] [2,4,6,8], half = 4
         
-        var l = 0
-        var r = a.count - 1
+        // make a the smaller array
+        let (a,b) = nums1.count <= nums2.count ? (nums1, nums2) : (nums2, nums1)
+        
+        // the index _at_ half is median for an odd total count. So if total count was 5, 2 is the middle element. For 6, the average of 2 and 3
+        let half = (a.count + b.count + 1) / 2
+        
+        var start = 0
+        var end = a.count
         
         while true {
-            let mid1 = (l + r) / 2
-            let mid2 = half - mid1 - 2
-            let aLeft = mid1 < 0 ? -Int.max : a[mid1]
-            let aRight = (mid1 + 1) >= a.count ? Int.max : a[mid1 + 1]
-            let bLeft = mid2 < 0 ? -Int.max : b[mid2]
-            let bRight = (mid2 + 1) >= b.count ? Int.max : b[mid2 + 1]
+            //[[2,4], [6,8]]
+            let mid1 = (start + end) / 2
             
-            if aLeft < bRight && bLeft < aRight {
-                if (nums1.count + nums2.count) % 2 == 0 {
-                    return (Double(max(aLeft, bLeft)) + Double(min(aRight, bRight)) / 2.0)
+            //[[1,3], [5,7,9]]
+            let mid2 = half - mid1 - 2
+            
+            let aLeft = mid1 < 0 ? Int.min : a[mid1]
+            let aRight = mid1 >= a.count - 1  ? Int.max : a[mid1 + 1]
+            let bLeft = mid2 < 0 ? Int.min : b[mid2]
+            let bRight = mid2 >= b.count - 1  ? Int.max : b[mid2 + 1]
+            
+            if aLeft <= bRight && bLeft <= aRight {
+                if (a.count + b.count) % 2 == 0 {
+                    return (Double(max(aLeft, bLeft)) + Double(min(aRight, bRight))) / 2.0
                 } else {
-                    return Double(min(aRight, bRight))
+                    return max(Double(aLeft), Double(bLeft))
                 }
             } else if aLeft > bRight {
-                r = mid1 - 1
-            } else if bLeft > aRight {
-                l = mid1 + 1
+                end = mid1 - 1
+            } else {
+                start = mid1 + 1
             }
         }
     }
--- MSFTInterviewQuestionsTests/MedianTwoSortedArraysTest.swift
+++ MSFTInterviewQuestionsTests/MedianTwoSortedArraysTest.swift
@@ -21,4 +21,19 @@ class MedianTwoSortedArraysTest: XCTestCase {
         let solver = MedianTwoSortedArrays()
         XCTAssertEqual(solver.findMedianSortedArrays([1,2], [3,4]), 2.5)
     }
+    
+    func testSolver3() {
+        let solver = MedianTwoSortedArrays()
+        XCTAssertEqual(solver.findMedianSortedArrays([1,3,5,7,9], [2,4,6,8]), 5)
+    }
+    
+    func testSolver4() {
+        let solver = MedianTwoSortedArrays()
+        XCTAssertEqual(solver.findMedianSortedArrays([1,3,5,7,9], [2,4,6,8,10]), 5.5)
+    }
+    
+    func testSolver5() {
+        let solver = MedianTwoSortedArrays()
+        XCTAssertEqual(solver.findMedianSortedArrays([3,4], [1,2]), 2.5)
+    }
 }
